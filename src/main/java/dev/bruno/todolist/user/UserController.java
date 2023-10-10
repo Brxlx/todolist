6efc998274b3a212496f3d7d7847cfb2c243ae46
Bruno Santos <brcesar19@gmail.com>
2023-10-10 12:27:26 -0300

    chore: response from response entity

--- src/main/java/dev/bruno/todolist/user/UserController.java
+++ src/main/java/dev/bruno/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package dev.bruno.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +16,16 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping()
-  public UserModel create(@RequestBody UserModel userModel) {
+  public ResponseEntity<String> create(@RequestBody UserModel userModel) {
     System.out.println(userModel.getName());
     var user = this.userRepository.findByUsername(userModel.getUsername());
 
-    if(user != null){
-      System.out.println("Usuário já existe.");
-      return null;
+    if (user != null) {
+      // Mensagem de erro
+      // Status Code
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe.");
     }
-
-    return this.userRepository.save(userModel);
+    this.userRepository.save(userModel);
+    return new ResponseEntity<>(HttpStatus.CREATED);
   }
 }
